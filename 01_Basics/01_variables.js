48519a374db3c4a0fcab774a5d4f43732ede2692
vikas8373 <70423770+vikas8373@users.noreply.github.com>
2023-10-26 08:15:31 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 14589
+let accountEmail = "vikas@gmail.com"
+var accountPassword = "45624"
+accountCity = "delhi"
+
+
+accountEmail = "verma@gmail.com"
+accountPassword = "24879169"
+accountCity = "bhopal"
+
+console.table([accountID,accountEmail, accountPassword, accountCity]);
+
+/* prefer not to use var beacause of issue in block scope and functional scope */ 
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as newer version
+// alert (3+6 ) //we r using nodejs not browser
+
+let name = "vikas"
+let age = "23"
+let isLoggedIn = false
+// premitive 
+// number = 2 to power53
+//big int
+// string ""
+// boolean => true/false
+
+// null=> standalone value
+
+// undefined =>
+
+// symbol => unique
+console.log(typeof "vikas");
+console.log(typeof undefined);
+console.log(typeof null);
