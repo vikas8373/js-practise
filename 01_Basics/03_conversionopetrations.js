bbda396e92e0f0a69c08c830cea670fc4534bbd8
vikas8373 <70423770+vikas8373@users.noreply.github.com>
2023-10-26 08:31:40 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionopetrations.js
@@ -0,0 +1,22 @@
+let score = "56bhghv"
+
+//console.log(typeof score);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "vikas" true
+ 
+let someNumber = 1544
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
