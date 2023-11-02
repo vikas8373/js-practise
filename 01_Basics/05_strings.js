214e981192a583f7db38ce3fc6bec2bec63b6dd2
vikas8373 <70423770+vikas8373@users.noreply.github.com>
2023-11-02 10:02:34 +0000

    strings in js

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,9 @@
+//  const name  = "VIKAS"
+//  const repoCount = 50 
+
+// //  console.log( name + repoCount + " value"); 
+
+// console.log(`HELLO MY NAME IS ${name} AND MY NAME IS ${repoCount}` );
+
+const gameName = new String('vicecity')
+console.log(gameName.indexOf('i'));
